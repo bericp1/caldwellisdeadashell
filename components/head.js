4d4de9f10ead1f8e9c147489ef9cfa37000937d8
Brandon Phillips <bericp1@gmail.com>
2017-10-27 10:14:21 -0600

    social media tags and images

--- components/head.js
+++ components/head.js
@@ -5,8 +5,10 @@ export default ({ children }) => (
     <title>Caldwell is Dead as Hell</title>
     <meta charSet='utf-8' />
     <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+
     <link rel="stylesheet" href="/static/css/normalize.min.css" />
     <link rel="stylesheet" href="/static/css/main.css" />
+
     <link rel="apple-touch-icon" sizes="180x180" href="/static/favicons/apple-touch-icon.png" />
     <link rel="icon" href="/static/favicons/favicon.ico" />
     <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
@@ -16,6 +18,28 @@ export default ({ children }) => (
     <link rel="manifest" href="/static/favicons/manifest.json" />
     <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#ff7f11" />
     <meta name="theme-color" content="#24292b" />
+
+    <!-- Search Engine -->
+    <meta name="description" content="Listen to What Should We Draw!" />
+    <meta name="image" content="https://caldwellisdeadashell.com/static/images/social-media-image.png" />
+    <!-- Schema.org for Google -->
+    <meta itemprop="name" content="Caldwell Is Dead As Hell" />
+    <meta itemprop="description" content="Listen to What Should We Draw!" />
+    <meta itemprop="image" content="https://caldwellisdeadashell.com/static/images/social-media-image.png" />
+    <!-- Twitter -->
+    <meta name="twitter:card" content="summary" />
+    <meta name="twitter:title" content="Caldwell Is Dead As Hell" />
+    <meta name="twitter:description" content="Listen to What Should We Draw!" />
+    <meta name="twitter:site" content="@bericp1" />
+    <meta name="twitter:image:src" content="https://caldwellisdeadashell.com/static/images/social-media-image.png" />
+    <!-- Open Graph general (Facebook, Pinterest & Google+) -->
+    <meta name="og:title" content="Caldwell Is Dead As Hell" />
+    <meta name="og:description" content="Listen to What Should We Draw!" />
+    <meta name="og:image" content="https://caldwellisdeadashell.com/static/images/social-media-image.png" />
+    <meta name="og:url" content="https://caldwellisdeadashell.com/" />
+    <meta name="og:site_name" content="Caldwell Is Dead As Hell" />
+    <meta name="og:type" content="website" />
+
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-108789514-1" />
     <script dangerouslySetInnerHTML={{__html: `
       window.dataLayer = window.dataLayer || [];
